514dbdce6df2fa35d62edb3ef860a4056513c5ed
xhe <xw897002528@gmail.com>
2020-08-15 19:58:49 +0800

    use iwd

--- ports/slstatus/config.h
+++ ports/slstatus/config.h
@@ -11,9 +11,9 @@ static const char unknown_str[] = "n/a";
 
 static const struct arg args[] = {
 	/* function	format        argument */
-	{ wifi_essid,    "%s ",                    "wlp3s0" },
-	{ netspeed_rx,   "| %s ",                  "wlp3s0" },
-	{ netspeed_tx,   " %s ",                    "wlp3s0" },
+	{ wifi_essid,    "%s ",                    "wlan0" },
+	{ netspeed_rx,   "| %s ",                  "wlan0" },
+	{ netspeed_tx,   " %s ",                    "wlan0" },
 	{ cpu_perc,      "| %s ",									  NULL },
 	{ cpu_freq,      " %s ",                   NULL },
 	{ ram_perc,      "| %s ",										NULL },
