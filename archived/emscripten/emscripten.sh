a1b1c3263c06c00caec0d70fde03921aea558ca4
xhe <xw897002528@gmail.com>
2020-10-22 13:29:00 +0800

    update
    Signed-off-by: xhe <xw897002528@gmail.com>

--- archived/emscripten/emscripten.sh
+++ archived/emscripten/emscripten.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-export EMSCRIPTEN="/lib/emscripten"
-export EMSCRIPTEN_FASTCOMP="/lib/emscripten-fastcomp"
-
-# add to path
-export PATH=$PATH:$EMSCRIPTEN
+export PATH="$PATH:/lib/emscripten"
