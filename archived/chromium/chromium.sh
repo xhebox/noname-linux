d69af4e137f7d5efc4d8a3427890ed01a0f4cceb
xhe <xw897002528@gmail.com>
2020-12-28 23:48:34 +0800

    update
    Signed-off-by: xhe <xw897002528@gmail.com>

--- archived/chromium/chromium.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-unset GDK_BACKEND
-export CHROME_WRAPPER=/lib/chromium/chromium
-export CHROME_DESKTOP=chromium.desktop
-export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
-exec /lib/chromium/chromium $CHROME_FLAGS "$@"
