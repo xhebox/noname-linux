71cc076c877ee14714a33866ea983c22f6c08fc7
xhe <xw897002528@gmail.com>
2022-01-25 01:56:05 +0800

    update
    Signed-off-by: xhe <xw897002528@gmail.com>

--- ports/musl/tmpl.h
+++ ports/musl/tmpl.h
@@ -1,6 +1,5 @@
-#if !defined __x86_64__
+#if defined(i386) || defined(__i386__)
 # include <__target32/bits/__name>
-#endif
-#if defined __x86_64__ && defined __LP64__
+#else
 # include <__target64/bits/__name>
 #endif
